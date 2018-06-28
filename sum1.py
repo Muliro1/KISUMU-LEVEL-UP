7074a7fae3440dea3a0496642110ddb96a8a5f5e
Muliro1 <mulirokhaemba@gmail.com>
2018-06-27 22:26:46 -0700

    Update sum1.py

--- sum1.py
+++ sum1.py
@@ -2,12 +2,10 @@ def addition(num1, num2):
 	'''
 	This function takes in two integer arguments and returns the sum
 	'''
-	if not isinstance(num1, int) or not isinstance(num2, int):
-		return('both arguments must be integers')
 	try:
 		return num1 + num2
 	except:
-		print('An Error has occurred')
+		print('An Error has occurred, both inputs must either be integers or floats')
 
 
 sum(1,3)
