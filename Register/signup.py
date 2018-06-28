a5ac0f9eba29f19755e3f4535c2c2ef342910203
Muliro1 <mulirokhaemba@gmail.com>
2018-06-27 17:03:07 -0700

    adding comments and some additional functionality

--- Register/signup.py
+++ Register/signup.py
@@ -1,22 +1,27 @@
 class Signup():
-	i = 0
-	user_info = []
-	def __init__(self, email, firstname, lastname, username, password):
+	user_id = 0
+	user_info = {'email':[], 'names':[], 'password':[]}
+	def __init__(self, email, firstname, lastname, password):
 		self.email = email
 		self.firstname = firstname
 		self.lastname = lastname
-		self.username = username
 		self.password = password
-		self.i += 1
+		self.user_id += 1
 	def set_email(self):
-		self.user_info.append({'email':self.email})
-	def set_names(self):
-		self.user_info[0].update({'names':self.firstname +" " +self.lastname})
-	def set_username(self):
-		self.user_info[0].update({'username':self.username})
+		self.user_info['email'].append(self.email)
+	def set_password(self):
+		self.user_info['password'].append(self.password)
+	def set_name(self):
+		self.user_info['names'].append(self.firstname + ' ' + self.lastname)
+	
+class LogIn(Signup):
+	def verify(self, email, password):
+		if self.email in self.user_info['email'] and self.password in self.user_info['password']:
+			return 'You are now logged in as {}'.format(self.user_info['names'][self.user_id])
+
+ 
 
 
-class Login(Signup):
 	
 	
 
@@ -29,4 +34,3 @@ class Login(Signup):
 
 
 
- #self.items.update({item_name: quantity})
